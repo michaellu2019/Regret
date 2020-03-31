54134ff484f3e928899f829c7fde4f73bfe3f025
michaellu2019 <mlu0708@mit.edu>
2020-03-31 02:33:49 -0400

    hide db config vars

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -6,11 +6,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-# app.config['SECRET_KEY'] = '0555i8hdjb13ae0qzdfdd1284ba180'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b58ebd7db8ff0c:bac21d3a@us-cdbr-iron-east-01.cleardb.net/heroku_ebeccc9572a0df3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://michaellu2019:Squatifa7.@regret-db.cdanwlm18f6n.us-east-2.rds.amazonaws.com/regret_db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', '')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', '')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
