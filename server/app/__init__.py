a5109eff62fd67b49e2aee8e3b9ee82c4c036370
michaellu2019 <mlu0708@mit.edu>
2020-03-31 20:28:53 -0400

    concealed db uri

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -6,8 +6,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://michaellu2019:Squatifa7.@regret-db.cdanwlm18f6n.us-east-2.rds.amazonaws.com/regret_db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', '')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', '')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
